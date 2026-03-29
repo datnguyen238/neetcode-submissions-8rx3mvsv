d0a36ff7b986b9335c72be04e48062753a9042ef
Dat <105346033+datnguyen238@users.noreply.github.com>
2026-03-29 16:13:39 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = deque()
+        self.mini = []
+
+
+    def push(self, val: int) -> None:
+        self.stack.appendleft(val)
+        if self.mini:
+            val = min(val, self.mini[-1])
+        self.mini.append(val)
+            
+    def pop(self) -> None:
+        self.stack.popleft()   
+        if self.mini: 
+            self.mini.pop()
+
+    def top(self) -> int:
+        return self.stack[0]
+        
+    def getMin(self) -> int:
+        return self.mini[-1]
+        
+
+        
